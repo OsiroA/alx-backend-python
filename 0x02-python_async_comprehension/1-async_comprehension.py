684f90b83b625cfe6389a8474454f16ec24e2819
OsiroA <124213013+OsiroA@users.noreply.github.com>
2023-12-12 21:51:29 +0100

    Update 1-async_comprehension.py.

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,7 +4,7 @@ Task 1's module.
 """
 from typing import List
 
-async_generator = using('0-async_generator').async_generator
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
